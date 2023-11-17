710d0194fe998dfcc0b852f99bc14cbdde0ce17f
matiss-norenbergs <97153551+matiss-norenbergs@users.noreply.github.com>
2023-11-17 23:25:52 +0200

    Added Tauri to the app

--- blazor-components/Program.cs
+++ blazor-components/Program.cs
@@ -4,8 +4,8 @@ using blazor_components;
 using blazor_components.Components;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-//builder.RootComponents.Add<App>("#app");
-//builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.RootComponents.RegisterCustomElement<Calculator>("blazor-calculator");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
---
+++ src/core/App.jsx
@@ -0,0 +1,11 @@
+import styles from "./App.module.css"
+
+function App() {
+    return (
+        <div>
+            <blazor-calculator />
+        </div>
+    )
+}
+
+export default App
